19cf991267f3c7767b69ab61c16143d74a909a9c
F4k1ng1t <156473484+F4k1ng1t@users.noreply.github.com>
2024-10-24 23:39:30 -0700

    initial

---
+++ RPG Plus/Assets/Scripts/SlashBehavior.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class SlashBehavior : MonoBehaviour
+{
+    SpriteRenderer slash;
+    BoxCollider2D hitbox;
+    private void Start()
+    {
+        slash = this.GetComponent<SpriteRenderer>();
+        hitbox = this.GetComponent<BoxCollider2D>();
+        slash.enabled = false;
+    }
+    public void HideSlash()
+    {
+        slash.enabled = false;
+        hitbox.enabled = false;
+    }
+    public void ShowSlash()
+    {
+        slash.enabled = true;
+        hitbox.enabled = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // get the enemy and damage them
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            enemy.photonView.RPC("TakeDamage", RpcTarget.MasterClient, this.GetComponentInParent<PlayerController>().damage);
+        }
+    }
+}
